9fe4e30b2689e281545ecf339e09aceea0f2b279
DingdingOvO <dq12345678_2024@qq.com>
2026-08-30 00:42:21 +0800

    fix: add typeof module guard to nanolog browser.js

--- packages/nanolog/browser.js
+++ packages/nanolog/browser.js
@@ -1,12 +1,12 @@
-const createLog = (namespace = '') => {
+const createLog = (namespace = ) => {
     const log = (childNamespace) => createLog(namespace ? `${namespace} ${childNamespace}` : childNamespace);
 
     const formattedNamespace = namespace ? [namespace] : [];
 
-    log.debug = log.log = console.debug.bind(console, ...formattedNamespace, 'debug');
-    log.info = console.log.bind(console, ...formattedNamespace, 'info')
-    log.warn = log.warning = console.warn.bind(console, ...formattedNamespace, 'warn')
-    log.error = console.error.bind(console, ...formattedNamespace, 'error');
+    log.debug = log.log = console.debug.bind(console, ...formattedNamespace, debug);
+    log.info = console.log.bind(console, ...formattedNamespace, info)
+    log.warn = log.warning = console.warn.bind(console, ...formattedNamespace, warn)
+    log.error = console.error.bind(console, ...formattedNamespace, error);
 
     return log;
 };
@@ -16,4 +16,4 @@ const createLog = (namespace = '') => {
  */
 createLog.enable = createLog.disable = () => {};
 
-module.exports = createLog;
+if(typeof module!=="undefined")module.exports=createLog;
